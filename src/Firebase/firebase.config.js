f1b76479fded071a106cd4990e378470156efe96
Rukonju <rukonbds@gmail.com>
2021-10-27 23:03:40 +0600

    firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: "AIzaSyDO3Q1lNfHUFacAB3jSyhZQcp2S7HDyzGI",
-    authDomain: "ema-jon-firebase.firebaseapp.com",
-    projectId: "ema-jon-firebase",
-    storageBucket: "ema-jon-firebase.appspot.com",
-    messagingSenderId: "1013899722886",
-    appId: "1:1013899722886:web:bd87281839af7cb7b8d107"
+    apiKey: "AIzaSyC4jQJJtGYkhS9RMguEvMGog1oprQ8sQ88",
+    authDomain: "ema-jhon-with-node.firebaseapp.com",
+    projectId: "ema-jhon-with-node",
+    storageBucket: "ema-jhon-with-node.appspot.com",
+    messagingSenderId: "1023073855612",
+    appId: "1:1023073855612:web:a6d9aed196109de6dc81f9"
 };
 
 export default firebaseConfig;
